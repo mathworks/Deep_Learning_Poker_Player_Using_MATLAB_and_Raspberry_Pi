1be5022708724d3dd20fb6bc5f2e63d406e616ef
Meeshawn Marathe <61815418+meeshawn-MW@users.noreply.github.com>
2020-03-23 15:11:05 +0530

    Create freezeWeights.m

---
+++ Poker_Setup/freezeWeights.m
@@ -0,0 +1,16 @@
+function layers = freezeWeights(layers)
+
+% layers = freezeWeights(layers) sets the learning rates of all the
+% parameters of the layers in the layer array |layers| to zero.
+
+for ii = 1:size(layers,1)
+    props = properties(layers(ii));
+    for p = 1:numel(props)
+        propName = props{p};
+        if ~isempty(regexp(propName, 'LearnRateFactor$', 'once'))
+            layers(ii).(propName) = 0;
+        end
+    end
+end
+
+end
